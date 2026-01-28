3436e1a35d0bfe2c378441615084c6584a095adc
NicolasM <ctc.group.plc.mdm@gmail.com>
2026-01-28 12:38:08 +0200

    Create authConfig.jsx

---
+++ src/authConfig.jsx
@@ -0,0 +1,18 @@
+export const msalConfig = {
+  auth: {
+    clientId: "9370acb0-2914-4445-84f2-120e196ef5b8", // App registration ID
+    authority: "https://ctcppl.b2clogin.com/ctcppl.onmicrosoft.com/v2.0/.well-known/openid-configuration?p=B2C_1_SignIn",
+    knownAuthorities: ["ctcppl.b2clogin.com/"],
+    redirectUri: "https://nice-smoke-0583ee103.2.azurestaticapps.net",
+    postLogoutRedirectUri: "https://nice-smoke-0583ee103.2.azurestaticapps.net"
+  }
+};
+
+export const loginRequest = {
+  scopes: [
+    "openid",
+    "profile",
+    "offline_access"
+    
+  ]
+};
